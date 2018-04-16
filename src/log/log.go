be4e209d9dd9a3d93fad7febd28062c46d42fd10
Vince0000 <522341976@qq.com>
2018-04-16 19:33:45 +0800

    Update log.go

--- src/log/log.go
+++ src/log/log.go
@@ -186,7 +186,7 @@ func (l *Logger) Print(v ...interface{}) { l.Output(2, fmt.Sprint(v...)) }
 
 // Println calls l.Output to print to the logger.
 // Arguments are handled in the manner of fmt.Println.
-func (l *Logger) Println(v ...interface{}) { l.Output(2, fmt.Sprintln(v...)) }
+func (l *Logger) Println(v ...interface{}) { l.Output(l.calldepth, fmt.Sprintln(v...)) }
 
 // Fatal is equivalent to l.Print() followed by a call to os.Exit(1).
 func (l *Logger) Fatal(v ...interface{}) {
